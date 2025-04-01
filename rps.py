617301ea4b93ebc11dd1ef2cf92cfc18e7a15530
000920890-Nicholas-Gates <nicholas.gates1@mohawkcollege.ca>
2025-03-31 20:06:10 -0400

    Added loop functionality and pinpointed bug to fix

--- rps.py
+++ rps.py
@@ -4,25 +4,31 @@
 
 import random
 
-user_action = input("Enter throw (rock, paper, scissors): ")
-ai_action = random.choice(["rock", "paper", "scissors"])
+runtime = True
 
-print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+while runtime == True:
+    user_action = input("Enter throw (rock, paper, scissors): ")
+    ai_action = random.choice(["rock", "paper", "scissors"])
 
-if user_action == ai_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "rock":
-    if ai_action == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "paper":
-    if ai_action == "paper":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "scissors":
-    if ai_action == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "paper": #Bug here
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+    user_input = input("Press (q) to quit, or anything else to play again!")
+    if user_input == "q":
+        runtime = False
